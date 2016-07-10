bd561129f4702c6cb0dc65b7c2c294451c8f97f3
Navjot Singh <weavebytes@gmail.com>
2016-07-10 15:34:46 +0530

    fn to process IAI

--- utils.py
+++ utils.py
@@ -9,7 +9,20 @@ def get_ip_prefix(ip):
     l = ip.split(".")
     return "%s.%s.%s" % (l[0], l[1], l[2])
 
+def process_IAI(msg):
+    status = True
+    try:
+        l = msg[3:].split(":")
+        return True, l[0], l[1]
+    except:
+        return False, "", ""
 
 if __name__ == "__main__":
     ip = "192.168.1.9"
     print get_ip_prefix(ip)
+
+    # good
+    print process_IAI("IAI1.2.3.4:navi")
+
+    # not good
+    print process_IAI("IAI1.2.3.4:navi")
