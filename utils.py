2d5058709ce5a959c5c0911b5bc5c38c5405f141
Navjot Singh <weavebytes@gmail.com>
2016-07-10 14:03:10 +0530

    utility to get IP prefix

--- utils.py
+++ utils.py
@@ -4,3 +4,12 @@ def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
+
+def get_ip_prefix(ip):
+    l = ip.split(".")
+    return "%s.%s.%s" % (l[0], l[1], l[2])
+
+
+if __name__ == "__main__":
+    ip = "192.168.1.9"
+    print get_ip_prefix(ip)
